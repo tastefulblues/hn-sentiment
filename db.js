30e81633709af1d10e9ab50fa7cf2ef34d3aca96
tastefulblues <maxjh28@gmail.com>
2026-06-05 14:09:33 -0600

    DB connection

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'hn_sentiment',
-    user: 'postgres',
-    password: process.env.DB_PASSWORD
+    connectionString: process.env.DATABASE_URL || `postgresql://postgres:${process.env.DB_PASSWORD}@127.0.0.1:5432/hn_sentiment`,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
